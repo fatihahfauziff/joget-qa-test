0358bfd1e94d1579b3f15484b04d80c4423d05fb
nurfatihahahmadfauzi <nurfatihahahmadfauzi@gmail.com>
2024-09-23 03:20:16 +0800

    Updated comment

--- joget-js/question4.js
+++ joget-js/question4.js
@@ -1,6 +1,8 @@
 let sequence = [1,2]
+
+// Limit to 5 datasets
 for (let i=0; i<5; i++){
-    let formula = i % 2 === 0 ? sequence[i]*sequence[i+1] : sequence[i]+sequence[i+1]
+    let formula = i % 2 === 0 ? sequence[i]*sequence[i+1] : sequence[i]+sequence[i+1] // Switch formula when iterator is either odd or even
     
     sequence.push(formula)
 }
